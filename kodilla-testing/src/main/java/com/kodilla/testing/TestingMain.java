60bf8721e53ff99e9f5c0180c5ddd0a22ac21c91
Anna Toczek <ania4134@gmail.com>
2022-02-10 12:25:07 +0000

    6.2 Test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+
+        Calculator calculator = new Calculator(8,10);
+
+        int resultAdd = calculator.add();
+        int resultSubtract = calculator.subtract();
+
+        if(resultAdd == 8+10) {
+            System.out.println("Add test ok");
+        }else{
+            System.out.println("Error!");
+        }
+
+        if(resultSubtract == 8-10) {
+            System.out.println("Subtract test ok");
+        }else{
+            System.out.println("Error!");
+        }
+
     }
 }
