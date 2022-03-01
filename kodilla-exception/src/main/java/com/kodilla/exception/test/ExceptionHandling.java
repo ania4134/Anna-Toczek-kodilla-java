a042b8ac405b0af4ef97d8ca4022df2a7dbc2ef4
Anna Toczek <ania4134@gmail.com>
2022-03-01 18:40:09 +0000

    8.3 obsługa wyjątku. Usunięta zbędna linia

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -10,7 +10,6 @@ public class ExceptionHandling {
     public static void main(String[] args) {
         SecondChallenge secondChallenge = new SecondChallenge();
 
-
         try {
            secondChallenge.probablyIWillThrowException(2.0, 1.0);
         } catch (Exception e) {
