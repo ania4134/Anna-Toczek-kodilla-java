d35aef5ecad5884ebd2387af88e71042a6c65d1c
Anna Toczek <ania4134@gmail.com>
2022-05-01 20:02:20 +0100

    15.5 kompletujemy wlasnego BigMaca

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bun.java
@@ -0,0 +1,6 @@
+package com.kodilla.patterns.builder.bigmac;
+
+public enum Bun {
+    Z_SEZAMEM,
+    BEZ_SEZAMU;
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Ingredient.java
@@ -0,0 +1,12 @@
+package com.kodilla.patterns.builder.bigmac;
+
+public enum Ingredient {
+    SALATA,
+    CEBULA,
+    BEKON,
+    OGOREK,
+    PAPRYCZKI_CHILI,
+    PIECZARKI,
+    KREWETKI,
+    SER;
+}
